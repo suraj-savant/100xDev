8557b73058169b70718c61b3c942900380f348c1
Suraj Savant <surajsavant9990@gmail.com>
2024-08-18 12:31:39 +0530

    created first-react-app

---
+++ 06-frontend-foundation/first-react-app/src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import FirstComponent from "./00-FirstComponent";
+import WrapperComponent from "./01-WrapperComponent";
+import PropComponent from "./02-PropComponent";
+import ReactHookComponent from "./03-ReactHooks";
+import MemoHookComponent from "./04-MemoHook";
+
+export default function App() {
+    return <>
+    {/* <FirstComponent />
+    <WrapperComponent />
+    <PropComponent /> */}
+    {/* <ReactHookComponent /> */}
+    <MemoHookComponent />
+    </>
+}
