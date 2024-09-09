784573356be913ddc1f757cd11547a06d4a8bfa4
suraj-savant <surajsavant9990@gmail.com>
2024-09-09 20:21:51 +0530

    frontend-foundation

--- 03-frontend-foundation/first-react-app/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './base-components/App'
-
-createRoot(document.getElementById('root')).render(
-
-  <App />
-)
