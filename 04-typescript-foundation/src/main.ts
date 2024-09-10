bd5df60da82ada76db57a04b8296fd8c8a0adb50
suraj-savant <surajsavant9990@gmail.com>
2024-09-10 11:16:21 +0530

    typescript sample code

---
+++ 04-typescript-foundation/src/main.ts
@@ -0,0 +1,66 @@
+// https://projects.100xdevs.com/tracks/6SbPPXGkG8QKFOTW9BmL/ts-2
+
+// let msg:string = "Hello World";
+
+// console.log(msg);
+
+// function sum(num1:number, num2:number):number{
+//     return num1 + num2;
+// }
+
+// console.log(sum(3,4))
+
+// function calculate(fn:(a:number, b:number)=>number, a:number, b:number){
+//     return fn(a, b);
+// }
+
+// function sum(num1:number, num2:number):number{
+//     return num1 + num2;
+// }
+
+// function product(num1:number) : number{
+//     return num1 * num1;
+// }
+
+// console.log(calculate(product,10, 5))
+
+// // // Use interface to define types
+
+// interface Person{
+//     name:string,
+//     age:number,
+//     isMarried:boolean,
+//     canVote?:()=>void
+// }
+
+// const user:Person= {
+//     name : "Suraj",
+//     age : 22,
+//     isMarried : false
+// }
+
+// class Employee implements Person{
+//     name: string;
+//     age: number;
+//     isMarried: boolean;
+   
+//     constructor(name:string, age:number, isMarried:boolean){
+//         this.name = name;
+//         this.age = age;
+
+//         this.isMarried = isMarried;
+//     }
+
+//     canVote() {
+//         if(this.age>=18) console.log(`${this.name} can vote`);
+//         else console.log(`${this.name} can't vote`);
+//     }
+// }
+
+// let emp1 : Employee = new Employee("Raju",18, false);
+// emp1.canVote();
+
+// console.log(user)
+
+//  use type to define types
+
